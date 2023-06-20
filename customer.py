aba9325f485acaef0a2a9893a2f2d78109fa3ead
netan <netaneld88@gmail.com>
2023-06-20 12:25:19 +0300

    try

---
+++ customer.py
@@ -0,0 +1,19 @@
+class Customer:
+    def __int__(self, name, customerID, contact_number):
+        self.__name = name
+        self.__customerID = customerID
+        self.__contact_number = contact_number
+
+    def chack_in(self):
+        pass
+
+    def chack_out(self):
+        pass
+    def modify_order(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    def last_visited(self):
+        pass
