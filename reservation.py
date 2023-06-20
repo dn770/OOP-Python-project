b179f32ed531de9e5877e703bf0df1bded7fd4ac
netan <netaneld88@gmail.com>
2023-06-20 12:30:43 +0300

    try

---
+++ reservation.py
@@ -0,0 +1,11 @@
+class Reservation:
+    def __int__(self, reservationID, reservation_time, number_of_people):
+        self.__reservationID = reservationID
+        self.__reservation_time = reservation_time
+        self.__number_of_people = number_of_people
+
+    def cancel_reservation(self):
+        pass
+
+    def change_reservation_time(self):
+        pass
