f5e970c9f64600e2fac3d8156980997ecbb74a9c
netan <netaneld88@gmail.com>
2023-06-28 20:20:53 +0300

    try

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ def main():
     description = input("please ")
     restaurant = Restaurant(name, address, phone_number, description)
     management_system = restaurant.create_management_system()
+    for num in range(int(input("please"))):
+        cap = int(input("insert capacity of table ",num))
+        management_system.add_table(cap)
+    for num in range(int(input("please"))):
+        int(input("insert  of menu ",num))
+        management_system.add_menu()
 
 
+
+main()
